ebbe7e482f081d26d96c86fbb224051ab2da0b33
ralexrivero <ralexrivero@gmail.com>
2022-06-12 14:22:37 -0300

    reorder files and create javscript folder

---
+++ javascript/01-number_string_booleans.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+/* template string */
+const firstName = 'Ronald';
+const age = '41';
+
+console.log(`${firstName}} is ${age} years old`); // for template string always use backtick "`"
